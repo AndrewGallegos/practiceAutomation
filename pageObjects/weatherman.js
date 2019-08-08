c334e1f1661bb733df23ebffe15bcfa1da30a831
AndrewGallegos <andrew.gallegos.professional@gmail.com>
2019-08-08 13:31:23 -0600

    weatherman testing

---
+++ pageObjects/weatherman.js
@@ -0,0 +1,11 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/weatherman/build/index.html',
+    elements: {
+        searchBar: '.enter-location__input',
+        searchButton: '.enter-location__submit',
+        searchAgainButton: '.current-weather__search-again',
+        resultCity: '.current-weather__location',
+        errorMessage: '.error-message__message',
+        tryAgainButton: '.error-message__try-again'
+    }
+}
